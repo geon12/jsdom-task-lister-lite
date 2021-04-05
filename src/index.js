66367b8060bd93cf34df404a6625c62dcceeca7d
Cameron <camcreel@gmail.com>
2021-04-04 22:13:15 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (event) =>{
+    const taskDescription = document.getElementById("new-task-description");
+    const li = document.createElement('li');
+    li.textContent = taskDescription.value;
+    const ul = document.querySelector('ul');
+    const select = document.getElementById('importance');
+    li.style.color = select.value;
+    ul.appendChild(li);
+    taskDescription.value = "";
+    event.preventDefault();
+  });
 });
